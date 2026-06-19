d4f740c668109f199abd3b42a711488fdcc2b188
Luchienlanh <157972111+Luchienlanh@users.noreply.github.com>
2026-06-19 14:14:37 +0700

    Delete srcV4 directory

--- srcV4/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-
-# srcV4 Lip-to-Speech Reconstruction Package
-__version__ = "4.0.0"
--- srcV4/data/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from srcV4.data.window_dataset import (
-    WindowedMelDataset,
-    collate_windows,
-    split_cache_files,
-)
-
-__all__ = ["WindowedMelDataset", "collate_windows", "split_cache_files"]
